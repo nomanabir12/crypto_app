a385070801bf67fdff389630fe27ae9bb4ab8175
nomanabir12 <alnomanofficial@gmail.com>
2021-11-15 15:23:03 +0600

    fetch api

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App';
 
+import { Provider } from 'react-redux';
+import App from './App';
+import store from './app/store';
 
 import 'antd/dist/antd.css';
 
@@ -12,7 +14,9 @@ import 'antd/dist/antd.css';
 
 ReactDOM.render(
     <Router>
+        <Provider store={store}>
         <App />
+      </Provider>
     </Router>, document.getElementById('root')
-    );
+);
 

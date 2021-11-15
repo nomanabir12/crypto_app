0ea01f0cfa182a4e7af5db4dc6c12ffd7352e1f5
nomanabir12 <alnomanofficial@gmail.com>
2021-11-15 18:04:28 +0600

    render data

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -5,6 +5,9 @@ import { Link } from 'react-router-dom';
 
 import { useGetCryptosQuery } from '../services/cryptoApi';
 
+import Cryptocurrencies from './Cryptocurrencies';
+import News from './News';
+
 
 const { Title } = Typography;
 
@@ -26,6 +29,16 @@ const Homepage = () => {
                 <Col span={12}><Statistic title="Total 24h Volume" value={`$${millify(globalStats.total24hVolume)}`} /></Col>
                 <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} /></Col>
             </Row>
+            <div className="home-heading-container">
+                <Title level={2} className="home-title">Top 10 Cryptos In The World</Title>
+                <Title level={3} className="show-more"><Link to="/cryptocurrencies">Show more</Link></Title>
+            </div>
+            <Cryptocurrencies simplified />
+            <div className="home-heading-container">
+                <Title level={2} className="home-title">Latest Crypto News</Title>
+                <Title level={3} className="show-more"><Link to="/news">Show more</Link></Title>
+            </div>
+            <News simplified />
         </>
     )
 }
